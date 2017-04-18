878b1ad22a9b73e53d7450e657d30cb9ef755dd4
Tim Sims <tsims@snowfly.com>
2017-04-17 22:00:10 -0600

    Finished splitDatabase (tested + all good)

--- database.go
+++ database.go
@@ -3,7 +3,7 @@ package main
 import (
     //"bufio"
 	"database/sql"
-    //"fmt"
+    "fmt"
     _ "github.com/mattn/go-sqlite3"
     "io"
     //"io/ioutil"
@@ -13,7 +13,7 @@ import (
 )
 
 func openDatabase (path string) (*sql.DB, error) {
-	dat, err := sql.Open( "sqlite3", path)
+	dat, err := sql.Open("sqlite3", path)
 
 	if err != nil || dat == nil {
 		return nil, err
@@ -24,7 +24,7 @@ func openDatabase (path string) (*sql.DB, error) {
 	pragma journal_mode = off;
 	`
 
-	_,err = dat.Exec(cmd)
+	_, err = dat.Exec(cmd)
 	if err != nil {
 		dat.Close()
 		return nil, err
@@ -33,7 +33,7 @@ func openDatabase (path string) (*sql.DB, error) {
 }
 
 func createDatabase(path string) (*sql.DB, error) {
-	if _,err := os.Stat(path); err == nil {
+	if _, err := os.Stat(path); err == nil {
 		os.Remove(path)
 	}
 	_, err := os.Create(path)
@@ -41,8 +41,7 @@ func createDatabase(path string) (*sql.DB, error) {
 		return nil, err
 	}
 
-	dat, err := sql.Open( "sqlite3", path)
-
+	dat, err := sql.Open("sqlite3", path)
 
 	if err != nil || dat == nil {
 		return nil, err
@@ -60,44 +59,85 @@ func createDatabase(path string) (*sql.DB, error) {
 		return nil, err
 	}
 	return dat, nil
-
 }
 
 func splitDatabase(source, outputPattern string, m int) ([]string, error) {
-	dat,err := openDatabase(source)
-
+	dat, err := openDatabase(source)
 	if err != nil || dat == nil {
 		return nil, err
 	}
+	defer dat.Close()
 
 	cmd := `
-	select count(1) from pairs
+	select count(1) from pairs;
 	`
-
-	rows,err := dat.Query(cmd)
+	row, err := dat.Query(cmd)
 	if err != nil {
-		dat.Close()
 		return nil, err
 	}
+	defer row.Close()
 	var i int;
-	if rows.Next() {
-		err = rows.Scan(&i)
+	if row.Next() {
+		err = row.Scan(&i)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		return nil, err
+		return nil, fmt.Errorf("No count returned from table pairs")
+	}
+	if i < m {
+		return nil, fmt.Errorf("Number of pairs < Number of Map tasks: i = %d, m = %d", i, m)
 	}
 
-	if i < m {
+	cmd = `
+	insert into pairs (key, value) values (?, ?);
+	`
+	filenames := make([]string, m)
+	dats := make([]*sql.DB, m)
+	inserts := make([]*sql.Stmt, m)
+	for i, _ := range(dats) {
+		filename := fmt.Sprintf(outputPattern, i)
+		filenames = append(filenames, filename)
+		dats[i], err = createDatabase(filename)
+		if err != nil {
+			return nil, err
+		}
+		defer dats[i].Close()
+		inserts[i], err = dats[i].Prepare(cmd)
+		if err != nil {
+			return nil, err
+		}
+		defer inserts[i].Close()
+	}
+
+	cmd = `
+	select key, value from pairs;
+	`
+	rows, err := dat.Query(cmd)
+	if err != nil {
 		return nil, err
 	}
-	//NOT FINISHED
-	return nil, nil
+	defer rows.Close()
+	var k, v string
+	i = 0
+	for rows.Next() {
+		err = rows.Scan(&k, &v)
+		if err != nil {
+			return nil, err
+		}
+		_, err = inserts[i].Exec(k, v)
+		if err != nil {
+			return nil, err
+		}
+		i = (i + 1) % m
+	}
+
+	return filenames, nil
 }
 
 func mergeDatabases(urls []string, path string, temp string) (*sql.DB, error) {
-	datbase, err := createDatabase(path)
+	// datbase, err := createDatabase(path)
+	_, err := createDatabase(path) // I changed this because it complained that datbase was unused. Just change it back whenever
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -133,9 +173,8 @@ func download(url, path string) error {
 }
 
 func main () {
-	//_,err := openDatabase("austen.sqlite3")
-	//_,err := createDatabase("datbase.sqlite3")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	_,err := splitDatabase("austen.sqlite3", "result-%d.sqlite3", 20)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
