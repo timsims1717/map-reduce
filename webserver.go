98a1ce5aa986e51260465f4ee1272de1b5f82d51
wdrummond <d00204186@dmail.dixie.edu>
2017-05-01 14:10:38 -0600

    stuffy stuff:

--- webserver.go
+++
@@ -1,8 +0,0 @@
-package main
-
-	go func() {
-    http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir("/data"))))
-    	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-    	    log.Printf("Error in HTTP server for %s: %v", "localhost:8080", err)
-   	 	}
-	}()
